73d5ada1817779b17eb7843cab39dd09ede91dea
Ahmed Mansour <154812224+AhmedMansour024@users.noreply.github.com>
2024-11-08 20:40:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import tkinter as tk
+from tkinter import filedialog
 import ttkbootstrap as ttk
 import yt_dlp as yt
 
 
+
+def choose_download_location():
+    global download_path
+    download_path = filedialog.askdirectory()
+    if download_path:
+        return download_path
+    return None
+
 def download_video():
     url = url_string.get()
     quality = quality_int.get()
-
+    
     if quality < 144:
         quality = 720
     
-    # Download options
-    ydl_opts = {
-        'format': f'best[height<={quality}]',
-        'outtmpl': '%(title)s.%(ext)s',
-        'ignoreerrors': True
-    }
+    if not download_path:
+        # Download options
+        ydl_opts = {
+            'format': f'best[height<={quality}]',
+            'outtmpl': '%(title)s.%(ext)s',
+            'ignoreerrors': True
+        }
+
+    else:
+        # Download options
+        ydl_opts = {
+            'format': f'best[height<={quality}]',                   # quality option
+            'outtmpl': f'{download_path}/%(title)s.%(ext)s',        # Save file with same video title
+            'ignoreerrors': True,                                   # skip unavailable videos and Ignore errors
+        }
 
     with yt.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
@@ -53,6 +71,8 @@ url = ttk.Entry(master=frame, width=50, textvariable=url_string)
 # quality entry
 quality_int = tk.IntVar()
 quality = ttk.Entry(master=frame, textvariable=quality_int)
+#download location button
+file_button = ttk.Button(master=frame, text="Download Location", command=choose_download_location)
 
 # button 
 button = ttk.Button(master=window, text="Download", command=download_video)
@@ -61,13 +81,14 @@ padlabel.pack()
 padframe.pack(pady=40)
 
 # pack frame_label
-url_label.pack(side='left', padx=90)
-quality_label.pack(side='left', padx=130)
+url_label.pack(side='left', padx=10)
+quality_label.pack(side='left', padx=210)
 frame_label.pack()
 
 # pack frame
 url.pack(side='left', padx=10)
 quality.pack(side='left')
+file_button.pack(side='left', padx=10)
 frame.pack(pady=5)
 
 # pack button
